ab3f7223d9951d53f833d0f1775d9244af1f7024
zhuiyizhiqiu <1124719223@qq.com>
2019-04-01 17:16:52 +0800

    Add fetching data from web service using synchronous network request

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+func < (lhs: SearchResult,rhs: SearchResult) -> Bool{
+    return lhs.name.localizedStandardCompare(rhs.name) == .orderedAscending
+}
 class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -35,25 +37,67 @@ class SearchViewController: UIViewController {
         searchBar.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK:- Private Methods
+    func iTunesURL(searchText: String) -> URL{
+        let endcodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@", endcodedText)
+        let url = URL(string: urlString)
+        return url!
+    }
 
+    func performStoreRequest(with url: URL) -> Data?{
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            print("Download Error: \(error.localizedDescription)")
+            showNetworkError()
+            return nil
+        }
+    }
+    
+    func parse(data: Data) -> [SearchResult] {
+        do{
+            let decoder = JSONDecoder()
+            let result = try decoder.decode(ResultArray.self, from: data)
+            return result.results
+        }catch{
+            print("JSON Error: \(error)")
+            return []
+        }
+    }
+    
+    func showNetworkError(){
+        let alert = UIAlertController(title: "Whoops.....", message: "There was an error accessing the iTunes Store." + "Please try again", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+    }
 
 }
 
 extension SearchViewController: UISearchBarDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-        searchResults = []
-        if searchBar.text! != "justin bieber"{
-            for i in 0...2 {
-                let searchResult = SearchResult()
-                searchResult.name = String(format: "Fake Result %d for", i)
-                searchResult.artistName = searchBar.text!
-                searchResults.append(searchResult)
+        if !searchBar.text!.isEmpty {
+            searchBar.resignFirstResponder()
+            
+            hasSearched = true
+            searchResults = []
+            
+            let url = iTunesURL(searchText: searchBar.text!)
+            print("URL:\(url)")
+            if let data = performStoreRequest(with: url){
+                searchResults = parse(data: data)
+                searchResults.sort(by: <)
+                searchResults.sort { (result1, result2) -> Bool in
+                    return result1.type.localizedStandardCompare(result2.type) == .orderedAscending
+                }
+                print("searchResult: \(searchResults)")
             }
+            tableView.reloadData()
         }
-        
-        hasSearched = true
-        tableView.reloadData()
     }
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
@@ -79,7 +123,11 @@ extension SearchViewController: UITableViewDelegate,UITableViewDataSource{
             let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCelllIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
             let searchResult = searchResults[indexPath.row]
             cell.nameLabel.text = searchResult.name
-            cell.artistNameLabel.text = searchResult.artistName
+            if searchResult.trackName.isEmpty{
+                cell.artistNameLabel.text = "Unknow"
+            }else {
+                cell.artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName!, searchResult.type)
+            }
             return cell
         }
     }
--- StoreSearch/Searchresult.swift
+++ StoreSearch/Searchresult.swift
@@ -6,7 +6,89 @@
 //  Copyright © 2019 彭军涛. All rights reserved.
 //
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+
+class ResultArray:Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
+}
+
+class SearchResult:Codable,CustomStringConvertible {
+    var artistName: String?
+    var trackName = ""
+    var kind: String?
+    var trackPrice: Double?
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
+    
+    enum CodingKeys: String,CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind,artistName,currency
+        case trackName,trackPrice,trackViewUrl
+        case collectionName,collectionViewUrl,collectionPrice
+    }
+    
+    var name: String{
+        return trackName ?? collectionName ?? ""
+    }
+    
+    var storeUrl: String{
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+    
+    var price: Double{
+        return trackPrice ?? collectionPrice ?? 0.0
+    }
+    
+    var genre: String{
+        if let genre = itemGenre {
+            return genre
+        }else if let genres = bookGenre{
+            return genres.joined(separator: ", ")
+        }
+        return ""
+    }
+    
+    var type: String {
+        let kind = self.kind ?? "audiobook"
+        switch kind {
+        case "album":
+            return "Album"
+        case "audiobook":
+            return "Audio Book"
+        case "book":
+            return "Book"
+        case "ebook":
+            return "E-Book"
+        case "feature-movie":
+            return "Move"
+        case "podcast":
+            return "Podcast"
+        case "software":
+            return "App"
+        case "song":
+            return "Song"
+        case "tv-episode":
+            return "TV Episode"
+        default:
+            break
+        }
+        return "Unknown"
+    }
+    
+    var description: String{
+        return "Kind: \(kind ?? ""),Name: \(name),Artist Name: \(artistName),genre: \(genre)"
+    }
 }
